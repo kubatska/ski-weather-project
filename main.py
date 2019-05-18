9360602915f75a6d2ff329a8ec09977f9e228790
kubatska <47230894+kubatska@users.noreply.github.com>
2019-05-18 13:42:08 +0300

    main.py

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ from SkiWeather import *
 
 
 def main():
-    """main function in program """
+    """main function in program
+    Return results."""
     country = str(input("\nPlease, enter a country, where you want to ski: "))
     date = str(input("Please, input a data in format yyyy-MM-dd: "))
+    if get_all_data(country, date) == "!":
+        return 
     get_all_data(country, date)
     all_data_end = convert_in()  # list with SkiWeather items
     
@@ -18,32 +21,34 @@ def main():
     for res in all_data_end:
         if res.name() != None:
             if res.whether() == [False] * 3:
-                print("\tIn {0}: the weather will not be suitable for skiing during all day".format(res.name()))
+                print("\tIn {0}: the weather will not be suitable for skiing during all day.".format(res.name()))
             elif res.whether() == [True] * 3:
-                print("\tIn {0}: the weather will be suitable for skiing during all day".format(res.name()))
+                print("\tIn {0}: the weather will be suitable for skiing during all day.".format(res.name()))
             else:
                 print("\tIn {0}:".format(res.name()))
                 if res.whether()[0] == True:
-                    print("\t\tin the morning the weather will be suitable for skiing")
+                    print("\t\tin the morning the weather will be suitable for skiing;")
                 if res.whether()[0] == False:
-                    print("\t\tin the morning the weather will not be suitable for skiing")
+                    print("\t\tin the morning the weather will not be suitable for skiing;")
                 if res.whether()[1] == True:
-                    print("\t\tin the afternoon the weather will be suitable for skiing")
+                    print("\t\tin the afternoon the weather will be suitable for skiing;")
                 if res.whether()[1] == False:
-                    print("\t\tin the afternoon the weather will not be suitable for skiing")
+                    print("\t\tin the afternoon the weather will not be suitable for skiing;")
                 if res.whether()[2] == True:
-                    print("\t\tin the eveningg the weather will be suitable for skiing".format(res.name()))
+                    print("\t\tin the eveningg the weather will be suitable for skiing.")
                 if res.whether()[2] == False:
-                    print("\t\tin the evening the weather will not be suitable for skiing".format(res.name()))
-            # print()
-    return "                                      Have a good rest!!!"
+                    print("\t\tin the evening the weather will not be suitable for skiing.")
+            
+    return "\n                                      Have a good rest!!!\n"
 
 
 def get_all_data(country, date):
+    '''(string, string) -> Nonetype
+    Get and write to file weather data.'''
     ch = check_date(date)
     if not ch or ch == None:
-        # print("You entered incongruous date")
-        return None
+        print("\nYou entered incongruous date\n")
+        return "!"
     if ch:
         key = "f83b6a4b839e4660b8691519190605"
         form = "json"
@@ -62,11 +67,11 @@ def get_all_data(country, date):
 
 def check_date(date):
     """(string) -> bool
-    Check if a date is in requisite boundaries"""
+    Check if a date is in requisite boundaries."""
     try:
         d_max = str(datetime.today() + timedelta(days=5))
         d_max = d_max[:10]
-        date_list = date.strip().split(".")
+        date_list = date.strip().split("-")[-1]
         d_max = d_max.strip().split("-")
         for i in range(3):
             if int(date_list[i]) > int(d_max[i]):
@@ -79,7 +84,7 @@ def check_date(date):
 
 def convert_in():
     """(json) -> Array(SkiWeather)
-    Сonvert data from a file to instances of the class"""
+    Сonvert data from a file to instances of the class."""
     
     f = open('data.json', encoding = 'utf-8')
     all_data = json.load(f)
